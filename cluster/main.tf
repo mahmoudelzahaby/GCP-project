cf33c8c2382f79bb715de6aa4dbcc21ca2397bd4
MahmoudDahaby <99130650+mahmouddahaby@users.noreply.github.com>
2023-02-25 23:03:32 +0200

    Update main.tf

--- cluster/main.tf
+++ cluster/main.tf
@@ -22,7 +22,7 @@ resource "google_container_cluster" "my-cluster" {
   master_authorized_networks_config {
     cidr_blocks {
       display_name = "manged-subnet"
-      cidr_block = "10.0.0.0/24"
+      cidr_block = "10.0.5.0/24"
     }
   }
 
