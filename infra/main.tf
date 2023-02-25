0d3b3127df5976f9788df18ac25bf6c45ac6017d
MahmoudDahaby <99130650+mahmouddahaby@users.noreply.github.com>
2023-02-25 14:05:50 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -83,10 +83,5 @@ resource "google_compute_firewall" "http" {
    router = google_compute_router.router.name
    region = google_compute_router.router.region
    nat_ip_allocate_option = "AUTO_ONLY"
-   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-
-   subnetwork {
-     name = google_compute_subnetwork.manged-subnet.id
-     source_ip_ranges_to_nat = [ "ALL_IP_RANGES" ]
-   }
+   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
  }
